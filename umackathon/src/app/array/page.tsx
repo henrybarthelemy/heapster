e98a94316fb6c16e9c942561ea2dc66221222b6e
Bacconatr <levin.alex2025@gmail.com>
2023-11-11 13:35:50 -0500

    more functionality to array + fixed animations

--- umackathon/src/app/array/page.tsx
+++ umackathon/src/app/array/page.tsx
@@ -3,10 +3,12 @@
 import React, { useEffect, useState } from 'react';
 import cytoscape from 'cytoscape';
 import "./style.css";
+import RuntimeBox from "@/app/runtime-box/runtimebox";
 
 
 const Cyto = () => {
-  const [cy, setCy] = useState(null);
+  const [cy, setCy] = useState<cytoscape.Core | null>(null);
+  const [nodeIndex, setNodeIndex] = useState<string>('');
 
   useEffect(() => {
     // Initialize Cytoscape
@@ -14,13 +16,13 @@ const Cyto = () => {
       container: document.getElementById('cy'),
       elements: [
         // nodes
-        { data: { id: 'item-1', label: '10' } },
-        { data: { id: 'item-2', label: '20' } },
-        { data: { id: 'item-3', label: '30' } },
+        { data: { id: 'item-0', label: '10' } },
+        { data: { id: 'item-1', label: '20' } },
+        { data: { id: 'item-2', label: '30' } },
         
         // edges
+        { data: { id: 'edge-0', source: 'item-0', target: 'item-1' } },
         { data: { id: 'edge-1', source: 'item-1', target: 'item-2' } },
-        { data: { id: 'edge-2', source: 'item-2', target: 'item-3' } },
 
         // edges can be removed if not needed
       ],
@@ -61,7 +63,10 @@ const Cyto = () => {
           selector: 'edge',
           style: {
             'width': 3,
-            'line-color': '#ccc'
+            'line-color': '#ccc',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+
           }
         },
         {
@@ -155,70 +160,144 @@ const Cyto = () => {
   };
   
 
+  const addNodeAtIndex = async (index: number) => {
+    // ... your existing logic to add a node, modified to use index
+
+    
+  };
+
+  const removeNodeAtIndex = (index: number) => {
+    if (!cy) return;
+  
+    const nodeIdToRemove = `item-${index}`;
+    const nodeToRemove = cy.getElementById(nodeIdToRemove);
+  
+    if (nodeToRemove.length > 0) {
+      // Get the predecessor and successor nodes
+      const previousNode = nodeToRemove.incomers().nodes();
+      const nextNode = nodeToRemove.outgoers().nodes();
+  
+      // Get all connected edges
+      const connectedEdges = nodeToRemove.connectedEdges();
+  
+      // Remove the node and the connected edges
+      connectedEdges.remove();
+      nodeToRemove.remove();
+  
+      // If there are nodes before and after the one that was removed, connect them
+      if (previousNode.length > 0 && nextNode.length > 0) {
+        cy.add({
+          group: "edges",
+          data: {
+            // Generate a new ID for the edge
+            id: `edge-${previousNode.id()}-${nextNode.id()}`,
+            source: previousNode.id(),
+            target: nextNode.id()
+          }
+        });
+      }
+
+      // now reindex the nodes
+      const nodes = cy.nodes();
+      nodes.forEach((node, index) => {
+        node.data('label', `${index + 1}`);
+      });
+  
+      // Reapply the layout if necessary
+      applyHorizontalLayout(cy);
+    } else {
+      console.warn(`No node found at index ${index}`);
+    }
+  };
+  
+  
+  const animateNode = async (nodeId: string, flashColor: string, originalColor: string, flashDuration: number) => {
+    const node = cy.getElementById(nodeId);
+    // Flash the node with the specified color
+    node.style('background-color', flashColor);
+    await node.animation({
+      style: { 'background-color': originalColor },
+      duration: flashDuration,
+      easing: 'ease-in-out-cubic'
+    }).play().promise();
+  };
 
   const addNodeAndEdge = async () => {
     if (!cy) return;
   
-    // Generate IDs for the new node and edge
+    // Calculate the new node ID based on the current number of nodes
     const totalNodes = cy.nodes().length;
-    const newNodeId = `item-${totalNodes + 1}`;
-    const label = `${totalNodes + 1}`;
-    const newEdgeId = `edge-${totalNodes + 1}-${totalNodes + 2}`;
+    const newNodeId = `item-${totalNodes}`;
   
-    // Add the new node and edge with initial opacity as 0 (invisible)
-    cy.add([
-      { group: 'nodes', data: { id: newNodeId, label: label }, style: { 'opacity': 0 } },
-      totalNodes > 0 ? { group: 'edges', data: { id: newEdgeId, source: `item-${totalNodes}`, target: newNodeId }, style: { 'opacity': 0 } } : null
-    ].filter(Boolean));
+    // Add the new node with initial opacity as 0 (invisible)
+    cy.add({
+      group: 'nodes',
+      data: { id: newNodeId, label: `${totalNodes + 1}` },
+      style: { opacity: 0 }
+    });
   
-    // Ensure new elements are ungrabbable
-    cy.$(`#${newNodeId}`).ungrabify();
-  
-    const flashDuration = 600;
-    const delay = 400;
-    const nodeFlashColor = 'red';
-    const lastNodeFlashColor = 'green'
-    const originalNodeColor = '#666'; // Assuming this is the original node color
-  
-    // Function to animate the node color
-
-    const flashNode = async (nodeId, flashColor, originalColor, flashDuration, delayDuration) => {
-      const node = cy.$(`#${nodeId}`);
-      await node.animation({
-        style: { 'background-color': flashColor },
-        duration: flashDuration / 2,
-        easing: 'ease-in-out'
-
-      }).play().promise();
-
-      await new Promise(resolve => setTimeout(resolve, delay));
-
-
-      await node.animation({
-        style: { 'background-color': originalColor },
-        duration: flashDuration / 2,
-        easing: 'ease-out' // Easing function for a smooth end
-      }).play().promise();
-    };  
-  
-    // Traverse and animate each node up to the new node
-    for (let i = 1; i <= totalNodes; i++) {
-      const currentNodeId = `item-${i}`;
-      await flashNode(currentNodeId, nodeFlashColor, originalNodeColor, flashDuration);
-    }
-  
-    // After the traversal animation, reveal the new node and edge
-    cy.$(`#${newNodeId}`).style('opacity', 1);
-    // turn the node green
-    cy.$(`#${newNodeId}`).style('background-color', lastNodeFlashColor);
+    // If this is not the first node, add an edge from the last node to the new node
     if (totalNodes > 0) {
-      cy.$(`#${newEdgeId}`).style('opacity', 1);
+      const newEdgeId = `edge-${totalNodes - 1}-${totalNodes}`;
+      cy.add({
+        group: 'edges',
+        data: {
+          id: newEdgeId,
+          source: `item-${totalNodes - 1}`,
+          target: newNodeId
+        },
+        style: { opacity: 0 }
+      });
     }
+  
+    // Ensure new elements are grabbable if needed
+    cy.getElementById(newNodeId).grabify();
+  
+    // Reveal the new node and edge
+    cy.getElementById(newNodeId).style('opacity', 0);
+    cy.getElementById(`edge-${totalNodes - 1}-${totalNodes}`).style('opacity', 0);
 
   
-    // Re-apply layout to place nodes correctly
+
+    
+    iterateNodes();
     applyHorizontalLayout(cy);
+
+    
   };
+
+  const iterateNodes = async () => {
+    const lastNodeIndex = cy.nodes().length - 1;
+  
+    // Iterate through all nodes
+    for (let i = 0; i < cy.nodes().length; i++) {
+      const currentNodeId = `item-${i}`;
+      
+      // Flash the current node
+      if (i < lastNodeIndex) {
+        // If it's not the last node, flash with red then back to original color
+        await animateNode(currentNodeId, 'red', '#666', 600);
+      } else {
+        // If it's the last node, flash with green and make sure it's visible
+        cy.getElementById(currentNodeId).style('opacity', 1);
+        
+        // Find the last edge connected to this node if it exists
+        if (i > 0) {
+          const lastEdgeId = `edge-${i - 1}-${i}`;
+          cy.getElementById(lastEdgeId).style('opacity', 1);
+        }
+  
+        await animateNode(currentNodeId, '#666', 'green', 800);
+      }
+  
+      // Add a delay between animations if necessary
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+  };
+  
+
+  
+  
 
   const removeLastNodeAndEdge = () => {
     if (!cy || cy.nodes().length < 1) return;
@@ -246,28 +325,87 @@ const Cyto = () => {
     // Re-apply layout
     applyHorizontalLayout(cy);
   };
+
+  const handleAddNodeClick = async () => {
+    if (nodeIndex.trim() === '') {
+      // If textbox is empty, call addNodeAndEdge directly
+      await addNodeAndEdge();
+    } else {
+      const index = parseInt(nodeIndex, 10);
+      if (!isNaN(index) && cy) {
+        await addNodeAtIndex(index);
+      }
+    }
+    // Re-apply layout to place nodes correctly
+    applyHorizontalLayout(cy);
+  };
+  
+  const handlePopNodeClick = () => {
+    if (nodeIndex.trim() === '') {
+      // If textbox is empty, call removeNodeAndEdge directly
+      removeLastNodeAndEdge();
+    } else {
+      const index = parseInt(nodeIndex, 10);
+      if (!isNaN(index) && cy) {
+        removeNodeAtIndex(index);
+      }
+    }
+    // Re-apply layout to place nodes correctly
+    applyHorizontalLayout(cy);
+  };
+  
   
 
   return (
-        <div>
-            <div className="title">
-                <p>Array</p>
-            </div>
-            <div className="description">
-                <p>
-                An array is a fundamental data structure that represents a collection of elements (values or variables), each identified by at least one array index or key. 
-                Arrays are used to store multiple values in a single variable, which allows for efficient access to elements based on their index. 
-                </p>
-            </div>
-            <div className="cyContainer">
-                <div id='cy' className="bg-blue-500" />
-            </div>
-            <div className="buttonBar">
-                <button onClick={addNodeAndEdge}>Add Node</button>
-                <button onClick={removeLastNodeAndEdge}>Pop Node</button>
-            </div>
+    <div>
+      <div className="title">
+        <p>Array</p>
       </div>
-  )
+      <div className="cyContainer">
+        <div id="cy" className="bg-blue-500" />
+      </div>
+      <div className="buttonBar">
+        <input
+            type="text" // You can also use type "number" here if you prefer
+            value={nodeIndex}
+            onChange={(e) => setNodeIndex(e.target.value)}
+            placeholder="Enter desired index (0 if empty)"
+            className="nodeIndexInput" // Make sure this class provides adequate styling
+            style = {{color: "black", }}
+
+          />
+
+        <button onClick={handleAddNodeClick}>Add Node</button>
+        <button onClick={handlePopNodeClick}>Pop Node</button>
+      </div>
+
+      <div className="fullParent">
+        <div className="halfChild">
+          <div className="description">
+            <p>
+              An array is a fundamental data structure that represents a
+              collection of elements (values or variables), each identified by
+              at least one array index or key. Arrays are used to store multiple
+              values in a single variable, which allows for efficient access to
+              elements based on their index.
+            </p>
+          </div>
+        </div>
+
+        <div className="halfChild">
+          <RuntimeBox stats={runtime}></RuntimeBox>
+        </div>
+      </div>
+    </div>
+  );
 };
 
+const runtime = {
+  "inserting / deletion of an element at the front" : "O(n)",
+  "inserting / deletion of an element at the end" : "O(1)",
+  "searching for an element" : "O(n)",
+
+}
+
 export default Cyto;
+
