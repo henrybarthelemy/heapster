0b71c3827d3625a22f7ede2299eec9af6abe80ad
henrybarthelemy <henry.v.barthelemy@gmail.com>
2023-11-11 17:32:15 -0500

    merge conflicts

--- umackathon/src/app/page.tsx
+++ umackathon/src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import Stack from "@/app/stack/page";
-<<<<<<< HEAD
 import NavBar from './Navbar';
 import Landing from './Landing';
 export default function Home() {
